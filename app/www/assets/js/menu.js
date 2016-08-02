25ec285ea5be5feaccd550ca483d28925b0ed6fb
Vu Anh Hao <vuanhhaogk@gmail.com>
2016-08-02 20:38:34 +0700

    Add close event to close button

--- app/www/assets/js/menu.js
+++ app/www/assets/js/menu.js
@@ -2,11 +2,16 @@
     var menu = document.getElementById('menu');
     var opmn = document.getElementById('openmenu');
     var item = menu.getElementsByTagName('a');
+    var clos = document.getElementById('titlebar').getElementsByClassName('close')[0];
 
     for (var i = 0; i < item.length; i++){
         item[i].onclick = function(){
             opmn.checked = false;
         }
+    }
+
+    clos.onclick = function(){
+        require('electron').remote.getCurrentWindow().close();
     }
 
 })();
