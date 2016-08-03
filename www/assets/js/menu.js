a0596f8efd1900d37a5019720d6df08a8dbe1c67
Vu Anh Hao <vuanhhaogk@gmail.com>
2016-08-03 18:30:48 +0700

    Add close menu when click outside

--- www/assets/js/menu.js
+++ www/assets/js/menu.js
@@ -3,6 +3,7 @@
     var opmn = document.getElementById('openmenu');
     var item = menu.getElementsByTagName('a');
     var clos = document.getElementById('titlebar').getElementsByClassName('close')[0];
+    var bmenu = document.getElementById('menu_bg');
 
     for (var i = 0; i < item.length; i++){
         item[i].onclick = function(){
@@ -12,6 +13,10 @@
 
     clos.onclick = function(){
         require('electron').remote.getCurrentWindow().close();
+    }
+
+    bmenu.onclick = function(){
+        opmn.checked = false;
     }
 
 })();
