54a32b1014aa96a1abc4ebd75a9f44867c9b7574
Vu Anh Hao <vuanhhaogk@gmail.com>
2016-08-03 20:36:52 +0700

    Change something in draw clock

--- www/assets/js/canvasclockServ.js
+++ www/assets/js/canvasclockServ.js
@@ -19,10 +19,18 @@ function updateFrame(h, m, s){
     this.render(this._stage);
 }
 
-function drawAxe(obj, step, len){
+function drawAxe(type, obj, radius, step, len, c_radius){
+    c_radius = c_radius || 0;
     for (var i = 0; i < 2 * Math.PI; i += step){
-        obj.moveTo(RADIUS * Math.cos(i), RADIUS * Math.sin(i));
-        obj.lineTo((RADIUS - len) * Math.cos(i), (RADIUS - len) * Math.sin(i));
+        switch (type){
+            case 'line':
+                obj.moveTo(radius * Math.cos(i), radius * Math.sin(i));
+                obj.lineTo((radius - len) * Math.cos(i), (radius - len) * Math.sin(i));
+                break;
+            case 'dot':
+                obj.drawCircle(radius * Math.cos(i), radius * Math.sin(i), c_radius);
+                break;
+        }
     }
 }
 
@@ -30,10 +38,19 @@ function initBackground(){
     var bg = new PIXI.Graphics();
 
     bg.position.set(CENTER_X, CENTER_Y);
-    bg.lineStyle(2, 0xececec, 1);
+    bg.lineStyle(1, 0xcecece, 1);
 
-    drawAxe(bg, Math.PI/6, MAX_AXE_LEN);
-    drawAxe(bg, Math.PI/30, MIN_AXE_LEN);
+    bg.drawCircle(0, 0, RADIUS);
+
+    drawAxe('line', bg, RADIUS - 3,  Math.PI/30, MIN_AXE_LEN);
+    bg.lineStyle(4, 0xcecece, 1);
+    drawAxe('line', bg, RADIUS - 1 - MAX_AXE_LEN, Math.PI/6, MAX_AXE_LEN);
+
+    bg.lineStyle(0, 0x000000, 0);
+    bg.beginFill(0xcecece, 1);
+    drawAxe('dot', bg, RADIUS - 3 - MIN_AXE_LEN/2, Math.PI/6, MIN_AXE_LEN, MIN_AXE_LEN/2);
+
+    bg.drawCircle(0, 0, 4);
 
     return bg;
 }
@@ -42,9 +59,9 @@ function createHand(l, w){
     var h = new PIXI.Graphics();
 
     h.position.set(CENTER_X, CENTER_Y);
-    h.lineStyle(w, 0xececec, 1);
-    h.moveTo(0, l/6);
-    h.lineTo(0, -l);
+    h.lineStyle(1, 0xececec, 1);
+    h.beginFill(0xececec, .4);
+    h.drawRect(-w/2, - l + l/6, w, l);
 
     return h;
 }
@@ -58,13 +75,13 @@ function initRenderer(){
         renderer._bg = initBackground();
         renderer._stage.addChild(renderer._bg);
 
-        renderer._hourhand = createHand(40, 4);
+        renderer._hourhand = createHand(60, 4);
         renderer._stage.addChild(renderer._hourhand);
 
-        renderer._minhand = createHand(80, 2);
+        renderer._minhand = createHand(100, 2);
         renderer._stage.addChild(renderer._minhand);
 
-        renderer._sechand = createHand(80, 1);
+        renderer._sechand = createHand(100, 1);
         renderer._stage.addChild(renderer._sechand);
 
         renderer.update = updateFrame;
